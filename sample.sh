6e6af6ad6a2adc976a4bdb01515984efa2d9cd52
Kumari Pratibha <kp939331@gmail.com>
2025-11-28 19:48:45 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,26 @@
+#!/bin/bash​
+
+​
+
+# Script: careersetup.sh​
+
+# Purpose: Automatically create a career planning
+folder structure.​
+
+​
+
+echo "Creating Career Folder Structure..."​
+
+​
+
+mkdir –
+p Career/{Certificates,Projects,Resumes,Notes,Hac
+kathons}​
+
+​
+
+echo "Folders created successfully!"​
+
+echo "Folder structure:"​
+
+ls Career
